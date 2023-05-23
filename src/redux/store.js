7f5576be0bcf2b29371e909b260041066e01de5b
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-05-23 14:30:23 +0530

    Add detail data slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import metricsReducer from './metrics/metricsSlice';
+import detailReducer from './details/detailsSlice';
 
 const store = configureStore({
   reducer: {
     metrics: metricsReducer,
+    detailData: detailReducer,
   },
 });
 
