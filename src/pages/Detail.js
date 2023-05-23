2167f485a57f2a5171e72b12dc40f77ef7f5e880
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-05-23 14:49:41 +0530

    Get detailData in page

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchDetailData } from '../redux/details/detailsSlice';
 
 function Detail() {
+  const dispatch = useDispatch();
+  const detailData = useSelector((store) => store.detailData.detailData);
+  const loading = useSelector((store) => store.detailData.isLoading);
+
+  useEffect(() => {
+    dispatch(fetchDetailData());
+  }, [dispatch]);
+
+  if (loading === true) {
+    return (
+      <h1 style={{ marginLeft: '40px' }}>Loading...</h1>
+    );
+  }
   return (
-    <div>Detail</div>
+    <div>{JSON.stringify(detailData)}</div>
   );
 }
 
--- src/redux/details/detailsSlice.js
+++ src/redux/details/detailsSlice.js
@@ -1,55 +1,55 @@
-import axios from "axios";
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from 'axios';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    detailData: {},
-    isLoading: true,
-}
+  detailData: {},
+  isLoading: true,
+};
 
 export const fetchDetailData = createAsyncThunk(
-    'metrics/detailData',
-    async (id) => {
-        try {
-            const response = await axios.get(`https://acnhapi.com/v1/art/${id}`);
-            const {
-                'buy-price': buyPrice, 'file-name': fileName, hasFake, image_uri: imageUrl, 'sell-price': sellPrice, 'museum-desc': museumDesc, name,
-            } = response.data;
-            const { 'name-EUen': nameEU } = name;
+  'metrics/detailData',
+  async (id = 3) => {
+    try {
+      const response = await axios.get(`https://acnhapi.com/v1/art/${id}`);
+      const {
+        'buy-price': buyPrice, 'file-name': fileName, hasFake, image_uri: imageUrl, 'sell-price': sellPrice, 'museum-desc': museumDesc, name,
+      } = response.data;
+      const { 'name-EUen': nameEU } = name;
 
-            const formatedData = {
-                buyPrice,
-                fileName,
-                hasFake,
-                imageUrl,
-                museumDesc,
-                sellPrice,
-                name,
-                nameEU,
-            };
+      const formatedData = {
+        buyPrice,
+        fileName,
+        hasFake,
+        imageUrl,
+        museumDesc,
+        sellPrice,
+        name,
+        nameEU,
+      };
 
-        return formatedData; 
-        } catch (error) {
-            return error.message;
-        }
-    },
+      return formatedData;
+    } catch (error) {
+      return error.message;
+    }
+  },
 );
 
 const detailDataSlice = createSlice({
-    name: 'detailData',
-    initialState,
-    reducers: [],
-    extraReducers: {
-      [fetchDetailData.fulfilled]: (state, action) => {
-        state.isLoading = false;
-        state.detailData = action.payload;
-      },
-      [fetchDetailData.rejected]: (state) => {
-        state.isLoading = false;
-      },
-      [fetchDetailData.pending]: (state) => {
-        state.isLoading = true;
-      },
+  name: 'detailData',
+  initialState,
+  reducers: [],
+  extraReducers: {
+    [fetchDetailData.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.detailData = action.payload;
     },
-  });
-  
-  export default detailDataSlice.reducer;
+    [fetchDetailData.rejected]: (state) => {
+      state.isLoading = false;
+    },
+    [fetchDetailData.pending]: (state) => {
+      state.isLoading = true;
+    },
+  },
+});
+
+export default detailDataSlice.reducer;
